4238cafdbfc0c41a1e44191d3f2aa33c5dcb43a2
Ben Marshall <benleem98@yahoo.com>
2024-08-05 18:14:42 -0400

    added basic markdown to html rendering

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,21 @@
 package routes
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
+
+type PageData struct {
+	Markdown string
+}
 
 func InitializePageRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
@@ -33,10 +44,38 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	md, err := convertMarkdown()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 	tmpl := template.Must(template.ParseFiles(IndexTemplates...))
-	err := tmpl.Execute(w, nil)
+	tmpl.New("markdown")
+	template.Must(tmpl.Lookup("markdown").Parse(md))
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		fmt.Printf("ERROR: %v\n", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func convertMarkdown() (string, error) {
+	source, err := os.ReadFile("public/static/assets/instructions.md")
+	if err != nil {
+		return "", err
+	}
+	md := goldmark.New(
+		goldmark.WithExtensions(extension.GFM, extension.Typographer),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+		),
+	)
+	var buf bytes.Buffer
+	if err := md.Convert(source, &buf); err != nil {
+		return "", err
+	}
+	result := buf.String()
+	return result, nil
 }
